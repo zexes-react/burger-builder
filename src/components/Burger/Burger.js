1351d0df4c92503622792b1acf0fc328b02a9a58
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-29 10:47:29 +0100

    Outputting Burger Ingredients Dynamically.closes #7

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = ( props ) => {
+   //Remember we are getting an object of key-value pairs from BurgerBuilder
+    //we are converting it to an array of BurgerBuilder
+    //* for each key in object, Note remember this can take (key, index) pair too
+    //# spread the value(int) as an array length
+    //$ for each index in Array now we ignore the element(_) but use the index(i)
+    //we build the <burgerIngredient/> with //key = salad2 unique key
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => { //*
+            return [...Array(props.ingredients[igKey])] //#
+                .map((_, i)=>{ //$
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
+
   return(
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top" />
-          <BurgerIngredient type="cheese" />
-          <BurgerIngredient type="meat" />
+          {transformedIngredients}
           <BurgerIngredient type="bread-bottom" />
       </div>
   );
