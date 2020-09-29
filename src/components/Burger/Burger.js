540f7e8da70d91f22477e5db6382989d565aa140
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-29 10:57:50 +0100

    Adding the Build Control Component. closes #8

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,14 +10,22 @@ const burger = ( props ) => {
     //# spread the value(int) as an array length
     //$ for each index in Array now we ignore the element(_) but use the index(i)
     //we build the <burgerIngredient/> with //key = salad2 unique key
-    const transformedIngredients = Object.keys(props.ingredients)
+    // to get all the inner arrays as one single array we use reduce... to see inner arrays, comment out reduce
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => { //*
             return [...Array(props.ingredients[igKey])] //#
                 .map((_, i)=>{ //$
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
 
+    console.log(transformedIngredients);
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
   return(
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top" />
