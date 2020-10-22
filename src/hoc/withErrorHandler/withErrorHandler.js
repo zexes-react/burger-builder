037e4c4974c8176d010f0de8f7ea1eee024581c2
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-22 22:00:27 +0100

    Removing Old Interceptors. closes #30

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,14 +4,15 @@ import Aux from '../Auxiliary/Auxiliary'
 
 const withErrorHandler = (WrapperComponent, axios) => {
     return class extends Component {
-
+        reqInterceptor;
+        resInterceptor;
         constructor(props, context) {// best to use interceptors in constructors
             super(props, context);
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null}) //clear errors
                 return req; //if you don't do this request will not continue
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             })
         }
@@ -28,6 +29,11 @@ const withErrorHandler = (WrapperComponent, axios) => {
         //         this.setState({error: error});
         //     })
         // }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
 
         errorConfirmedHandler = () =>{
             this.setState({error: null})
