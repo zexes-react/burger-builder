82b75bc2b9acc6ac12c4ca384fa45d9f04013e80
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-22 17:46:58 +0100

    Global Handling Errors. closes #28

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from '../Auxiliary/Auxiliary'
+
+const withErrorHandler = (WrapperComponent, axios) => {
+    return class extends Component {
+        state ={
+            error: null
+        }
+        componentDidMount() {
+            axios.interceptors.request.use(req => {
+                this.setState({error: null}) //clear errors
+                return req; //if you don't do this request will not continue
+            });
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error});
+            })
+        }
+
+        errorConfirmedHandler = () =>{
+            this.setState({error: null})
+        }
+
+        render() {
+            return <Aux>
+                <Modal show={this.state.error}
+                modalClosed={this.errorConfirmedHandler}>
+                    {this.state.error ? this.state.error.message : null}
+                </Modal>
+                <WrapperComponent {...this.props} />
+            </Aux>
+        }
+    }
+}
+
+export default withErrorHandler;
