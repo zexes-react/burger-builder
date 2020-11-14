30323c8ed687ba68ccd99d1c88b94d9ff94b6692
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-14 15:00:43 +0100

    Navigating to the Checkout Page. closes #32

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,6 +33,7 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://flash-chat-26a01.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
@@ -95,28 +96,29 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () =>{
         //alert('You Continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Ezekiel Eromosei',
-                address:{
-                    street: 'Teststreet1',
-                    zipCode: '41351',
-                    country: 'Germany'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Ezekiel Eromosei',
+        //         address:{
+        //             street: 'Teststreet1',
+        //             zipCode: '41351',
+        //             country: 'Germany'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout')
     }
 
     render(){
