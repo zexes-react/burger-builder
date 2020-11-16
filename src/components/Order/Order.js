49c73b3c96149c4ae617d474e7c330090fb8d355
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-16 16:37:13 +0100

    Outputting the Orders. closes #40

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-)
+const order = (props) => {
+    const ingredients = [];
+
+    // same as the one in the burger
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientsOutput = ingredients.map(ig => {
+        return <span key={ig.name}
+                     style={{textTransform: 'capitalize',
+                     display: 'inline-block',
+                     margin: '0 8px',
+                     border: 'ipx solid #ccc',
+                     padding: '5px'}}>
+            amount {ig.name} {ig.amount}
+        </span>
+    });
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
