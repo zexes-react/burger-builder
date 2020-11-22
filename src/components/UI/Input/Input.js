042c48960d312b3d0e204686b03a49c5ce620a21
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-22 22:53:30 +0100

    Adding validation feedback. closes #48

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,16 +4,22 @@ import classes from './Input.module.css';
 
 const input  = (props) => {
     let inputElement;
+    const inputClasses = [classes.InputElement];
+
+    if(props.invalid && props.shouldValidate){
+        console.log(props.invalid);
+        inputClasses.push(classes.Invalid);
+    }
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            inputElement = <textarea className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('select'):
-            inputElement = <select className={classes.InputElement} value={props.value} onChange={props.changed}>
+            inputElement = <select className={inputClasses.join(' ')} value={props.value} onChange={props.changed}>
                 {props.elementConfig.options.map(option => (
                     <option key={option.value} value={option.value}>
                         {option.displayValue}
@@ -22,7 +28,7 @@ const input  = (props) => {
             </select>;
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
     }
 
     return (
